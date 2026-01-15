04a9cc7811df729b802837661be0fc4f2618e1f7
Cristine <64547045+Ivy-Jiao@users.noreply.github.com>
2026-01-15 11:33:54 +1100

    react-1【fix】: resolve errors in income tax calculation

--- React-1/taxCalculate.js
+++ React-1/taxCalculate.js
@@ -1,25 +1,29 @@
-function calculateTax(income){
-    let rate = -1;
-    let tax = -1;
-    const rates = [0, 0.19, 0.325, 0.37, 0.45]
-    const incomes = [18200, 45000, 120000, 180000]
-    const taxes = [incomes[0]*rates[0], (incomes[1]-incomes[0])*rates[1], (incomes[2]-incomes[1])*rates[2], (incomes[3]-incomes[2])*rates[3]];
-    if(income <= incomes[0]){
-        rate = rates[0];
-        tax = income * rate;
+function calculateTax(income) {
+    const rates = [0, 0.19, 0.325, 0.37, 0.45];
+    const brackets = [18200, 45000, 120000, 180000];
+
+    let tax = 0;
+
+    if (income <= brackets[0]) {
+        tax = 0;
+    } else if (income <= brackets[1]) {
+        tax = (income - brackets[0]) * rates[1];
+    } else if (income <= brackets[2]) {
+        tax =
+            (brackets[1] - brackets[0]) * rates[1] +
+            (income - brackets[1]) * rates[2];
+    } else if (income <= brackets[3]) {
+        tax =
+            (brackets[1] - brackets[0]) * rates[1] +
+            (brackets[2] - brackets[1]) * rates[2] +
+            (income - brackets[2]) * rates[3];
+    } else {
+        tax =
+            (brackets[1] - brackets[0]) * rates[1] +
+            (brackets[2] - brackets[1]) * rates[2] +
+            (brackets[3] - brackets[2]) * rates[3] +
+            (income - brackets[3]) * rates[4];
     }
 
-    if(income <= incomes[1]){
-        rate = rates[1]
-        tax = (income-18200)*rate;
-    }
-    if (income <=incomes[2]) {
-        rate = rates[2];
-        tax = taxes[0]+taxes[1] + (income-45000)*rate;
-    }
-
-    if (income < income[3]) {
-        rate = rates[3]
-        tax = taxes[0] + taxes[1] + taxes[2] + (incomes[3]-incomes[2])*rates[2]
-    }
-};
+    return tax;
+}
