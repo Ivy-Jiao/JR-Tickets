45a5b9cfa44dec5d8fdc370b5786b997c3891280
Cristine <64547045+Ivy-Jiao@users.noreply.github.com>
2026-02-22 21:10:24 +1100

    express-1: get data from reques

--- 1-express/server.js
+++ 1-express/server.js
@@ -2,8 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello from express');
+// Middleware
+app.use(express.json());
+
+//app.{method}(pathname, route handler)
+// app.get('/', (req, res) => {
+//   res.send('hello from express');
+// });
+
+app.get('/movies/:id/:name', (req, res) => {
+  const {title, rating} = req.query;
+
+  res.send({
+    params: req.params,
+    query: {
+      title,
+      rating,
+    },
+    number: 123,
+  });
+
+});
+
+app.post('/movies', (req, res) => {
+  // res.send({body: req.body});
+  res.status(201).json({body: req.body});
 });
 
 app.listen(3000, () => {
